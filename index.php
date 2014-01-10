416a52312f21725163e9d556ab9fd1e1aef59e96
Daniel Morales <daniminas@gmail.com>
2014-01-10 13:27:53 -0300

    Adding composer file and a Silex type index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-phpinfo();
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+
+$app->get('/', function(){
+	return "Hello, Silex is now Running :)";
+});
+
+$app->run();
